dbe71f1bd8088f68b5df60787b6cbead23751631
WiDSatUBC <marketing.widsatubc@gmail.com>
2025-02-23 23:18:35 -0800

    team

--- components/Register.jsx
+++ components/Register.jsx
@@ -1,24 +1,20 @@
-import React from 'react'
+import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
 const Register = () => {
   return (
-    <div className='flex flex-col items-center justify-center items-center  text-center mt-3 md:mt-10 lg:mt-10'>
-      <p className='text-[8px] md:text-lg  tracking-widest  lg:text-2xl  text-key font-bold  '>WiDS X WiCS present a</p>
-      <p className='text-[8px] md:text-lg tracking-widest lg:text-2xl text-key font-bold  '>24-hour wellness-themed hackathon</p>
-      <p className='text-[8px] md:text-lg lg:text-2xl tracking-widest text-key font-bold '>April 6-7, 2024</p>
+    <div className='flex flex-col items-center justify-center text-center mt-3 md:mt-10 lg:mt-10'>
+      <p className='text-[8px] md:text-lg tracking-widest lg:text-2xl text-key font-bold'>WiDS X WiCS present a</p>
+      <p className='text-[8px] md:text-lg tracking-widest lg:text-2xl text-key font-bold'>24-hour wellness-themed hackathon</p>
+      <p className='text-[8px] md:text-lg lg:text-2xl tracking-widest text-key font-bold'>April 5-6, 2024</p>
       <Link href="" className='md:mt-2 lg:mt-8'>
-      <button className="bg-accent-magenta px-1 md:px-8 md:py-1 lg:py-3 rounded-full text-[10px] md:text-lg lg:text-2xl  transition-colors duration-300 hover:scale-105 transition duration-500">
-        <span className="mix-blend-screen">Apply Now</span>
-      </button>
-        {/* <p className='text-[8px] tracking-wider md:text-xl lg:text-2xl  px-7 rounded-3xl py-1 transform hover:scale-105 transition duration-500 drop-shadow-xl '>
-          Registration opens in March!
-        </p> */}
+        <button className="bg-accent-magenta px-1 md:px-8 md:py-1 lg:py-3 rounded-full text-[10px] md:text-lg lg:text-2xl text-white transition-colors duration-300 hover:scale-105">
+          Registration Opens Soon!
+        </button>
       </Link>
-
     </div>
-  )
+  );
 }
 
-export default Register
+export default Register;
