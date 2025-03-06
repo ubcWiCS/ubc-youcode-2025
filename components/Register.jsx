69c033c4e67f23e2465b687f7cf2dbc1c870dfd7
Beta-test-initiation <tanyabansal2203@gmail.com>
2025-03-05 20:55:33 -0800

    Changed logo

--- components/Register.jsx
+++ components/Register.jsx
@@ -4,9 +4,9 @@ import Link from 'next/link';
 const Register = () => {
   return (
     <div className='flex flex-col items-center justify-center text-center mt-3 md:mt-10 lg:mt-7'>
-      <p className='text-[8px] md:text-lg tracking-widest lg:text-2xl text-key font-bold'>WiDS X WiCS present a</p>
+      {/* <p className='text-[8px] md:text-lg tracking-widest lg:text-2xl text-key font-bold'>WiDS X WiCS present a</p>
       <p className='text-[8px] md:text-lg tracking-widest lg:text-2xl text-key font-bold'>24-hour hackathon</p>
-      <p className='text-[8px] md:text-lg lg:text-2xl tracking-widest text-key font-bold'>April 5th-6th, 2024</p>
+      <p className='text-[8px] md:text-lg lg:text-2xl tracking-widest text-key font-bold'>April 5th-6th, 2024</p> */}
 
       <Link href="https://ubc.ca1.qualtrics.com/jfe/form/SV_aXbg2iAkEk8uKI6" target="_blank" className='md:mt-2 lg:mt-8'>
         <button className="bg-accent-magenta px-1 md:px-8 md:py-1 lg:py-3 rounded-full text-[10px] md:text-lg lg:text-2xl text-white transition-colors duration-300 hover:scale-105">
--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -1,23 +1,19 @@
-import React from 'react'
-
-import Register from '@/components/Register';
-import Image from 'next/image';
-
-import logo from "../public/assets/logo.png";
+import Register from "@/components/Register";
+import Image from "next/image";
 
 const Hero = () => {
   return (
-    <div id="hero">
-      <div className="flex flex-col justify-center items-center">
-        <img
-          src= "/assets/logo.png"
-          alt="YouCode Logo"
-          className="w-100 "
-        />
-      </div>
-      <Register/>
+    <div id="hero" className="flex flex-col items-center justify-center">
+      <Image
+        src="/assets/logowithtext.png"
+        alt="YouCode Logo"
+        width={700} // Set your desired width
+        height={700} // Set your desired height
+        className="w-auto h-auto max-w-sm lg:max-w-lg" // Optional: Tailwind classes
+      />
+      <Register />
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
