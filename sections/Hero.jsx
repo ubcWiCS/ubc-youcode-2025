8c1e35ceff79fb83b0876a932942d9faf4e85f62
Beta-test-initiation <tanyabansal2203@gmail.com>
2025-03-06 01:11:22 -0800

    altered hero section

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -3,14 +3,17 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <div id="hero" className="flex flex-col items-center justify-center">
-      <Image
-        src="/assets/logowithtext.png"
-        alt="YouCode Logo"
-        width={2000} // Set your desired width
-        height={700} // Set your desired height
-        className="w-auto h-auto max-w-sm lg:max-w-lg" // Optional: Tailwind classes
-      />
+    <div id="hero" className="flex flex-col items-center justify-center w-full">
+      <div className="w-full  px-4"> {/* Adjust max-width as needed */}
+        <Image
+          src="/assets/logowithtext.png"
+          alt="YouCode Logo"
+          width={700} // Increase width
+          height={700} // Increase height
+          className="w-3/4 mx-auto h-auto" // Make the image responsive
+          priority // Optional: Prioritize loading this image
+        />
+      </div>
       <Register />
     </div>
   );
