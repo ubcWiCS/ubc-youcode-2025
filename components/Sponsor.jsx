f771abd267d1b173227e7c89cb9091957d74975b
carissaor <carissaor@gmail.com>
2026-02-26 15:33:41 -0800

    recap + sponsor fix

--- components/Sponsor.jsx
+++ components/Sponsor.jsx
@@ -6,9 +6,9 @@ const Sponsor = () => {
   return (
     <section id="sponsors" className="relative w-full bg-[#A1D4F2] -mb-2">
       <div className="absolute top-6 left-1/2 -translate-x-1/2 z-10">
-        {/* <h2 className="text-[var(--blue-dark)] font-dm italic text-3xl sm:text-4xl md:text-5xl drop-shadow-[0_4px_10px_rgba(0,0,0,0.1)]">
+        <h2 className="text-[var(--blue-dark)] font-dm italic text-3xl sm:text-4xl md:text-5xl drop-shadow-[0_4px_10px_rgba(0,0,0,0.1)]">
           Our 2026 Sponsors
-        </h2> */}
+        </h2>
       </div>
 
       {/* Background image */}
